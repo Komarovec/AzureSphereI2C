a8148e24ec7290d22e72f29001e5ef890b3ef945
komarovec <komarovec1@gmail.com>
2020-03-07 16:28:15 +0100

    BlueControl low level i2c

---
+++ HighLevelCore/constants.h
@@ -0,0 +1,60 @@
+// Motor pins SIDE:PIN:Forward/Backward --> LEFT1F Left First Forward
+#define LEFT1F 15 
+#define LEFT2F 11
+#define LEFT3F 3
+#define RIGHT1F 0
+#define RIGHT2F 8
+#define RIGHT3F 12
+
+#define LEFT1B 14
+#define LEFT2B 10
+#define LEFT3B 2
+#define RIGHT1B 1
+#define RIGHT2B 9
+#define RIGHT3B 13
+
+// I2C
+#define I2C_STRUCTS_VERSION 1
+#define I2C_BUS_TIMEOUT_MS      100
+
+// REGISTER ADDRESSES
+#define PCA9685_MODE1 0x00      /**< Mode Register 1 */
+#define PCA9685_MODE2 0x01      /**< Mode Register 2 */
+#define PCA9685_SUBADR1 0x02    /**< I2C-bus subaddress 1 */
+#define PCA9685_SUBADR2 0x03    /**< I2C-bus subaddress 2 */
+#define PCA9685_SUBADR3 0x04    /**< I2C-bus subaddress 3 */
+#define PCA9685_ALLCALLADR 0x05 /**< LED All Call I2C-bus address */
+#define PCA9685_LED0_ON_L 0x06  /**< LED0 on tick, low byte*/
+#define PCA9685_LED0_ON_H 0x07  /**< LED0 on tick, high byte*/
+#define PCA9685_LED0_OFF_L 0x08 /**< LED0 off tick, low byte */
+#define PCA9685_LED0_OFF_H 0x09 /**< LED0 off tick, high byte */
+// etc all 16:  LED15_OFF_H 0x45
+#define PCA9685_ALLLED_ON_L 0xFA  /**< load all the LEDn_ON registers, low */
+#define PCA9685_ALLLED_ON_H 0xFB  /**< load all the LEDn_ON registers, high */
+#define PCA9685_ALLLED_OFF_L 0xFC /**< load all the LEDn_OFF registers, low */
+#define PCA9685_ALLLED_OFF_H 0xFD /**< load all the LEDn_OFF registers,high */
+#define PCA9685_PRESCALE 0xFE     /**< Prescaler for PWM output frequency */
+#define PCA9685_TESTMODE 0xFF     /**< defines the test mode to be entered */
+
+// MODE1 bits
+#define MODE1_ALLCAL 0x01  /**< respond to LED All Call I2C-bus address */
+#define MODE1_SUB3 0x02    /**< respond to I2C-bus subaddress 3 */
+#define MODE1_SUB2 0x04    /**< respond to I2C-bus subaddress 2 */
+#define MODE1_SUB1 0x08    /**< respond to I2C-bus subaddress 1 */
+#define MODE1_SLEEP 0x10   /**< Low power mode. Oscillator off */
+#define MODE1_AI 0x20      /**< Auto-Increment enabled */
+#define MODE1_EXTCLK 0x40  /**< Use EXTCLK pin clock */
+#define MODE1_RESTART 0x80 /**< Restart enabled */
+// MODE2 bits
+#define MODE2_OUTNE_0 0x01 /**< Active LOW output enable input */
+#define MODE2_OUTNE_1                                                          \
+  0x02 /**< Active LOW output enable input - high impedience */
+#define MODE2_OUTDRV 0x04 /**< totem pole structure vs open-drain */
+#define MODE2_OCH 0x08    /**< Outputs change on ACK vs STOP */
+#define MODE2_INVRT 0x10  /**< Output logic state inverted */
+
+#define PCA9685_I2C_ADDRESS 0x41      /**< Default PCA9685 I2C Slave Address */
+#define FREQUENCY_OSCILLATOR 25000000 /**< Int. osc. frequency in datasheet */
+
+#define PCA9685_PRESCALE_MIN 3   /**< minimum prescale value */
+#define PCA9685_PRESCALE_MAX 255 /**< maximum prescale value */
--- HighLevelCore/main.c
+++ HighLevelCore/main.c
@@ -1,30 +1,271 @@
 ﻿#include <stdbool.h>
-#include <stdio.h>
 #include <errno.h>
 #include <string.h>
 #include <time.h>
-#include "MT3620_Grove_Shield_Library/Grove.h"
-#include "MT3620_Grove_Shield_Library/Sensors/GroveTempHumiSHT31.h"
-
+#include "constants.h"
+#include <applibs/i2c.h>
 #include <applibs/log.h>
 
-int main(void) {
-    int i2cFd;
-    
-    GroveShield_Initialize(&i2cFd, 115200); // baudrate - 9600,14400,19200,115200,230400 
+/*
+***** Low-level I2C
+*/
 
-    void* sht31 = GroveTempHumiSHT31_Open(i2cFd);
+/*!
+ *  @brief  Write one byte in specified register via I2C
+ *  @param _i2c I2C interface
+ *  @param  address Device address
+ *  @param  reg Address of the register
+ *  @param  val Value to be written (MUST BE A BYTE)
+ *  @return Number of bytes successfuly written
+ */
+static int writeReg8(int _i2c, uint8_t address, uint8_t reg, uint8_t val) {
+    uint8_t send[2];
+    send[0] = reg;
+    send[1] = val;
+    int er = I2CMaster_Write(_i2c, address, send, sizeof(send));
+    return er;
+}
 
+/*!
+ *  @brief  Read one byte in specified register via I2C
+ *  @param _i2c I2C interface
+ *  @param  address Device address
+ *  @param  reg Address of the register
+ *  @param  val pointer to returning value
+ *  @return 1 on succes; -1 otherwise
+ */
+static int readReg8(int _i2c, uint8_t address, uint8_t reg, uint8_t* val) {
+    I2CMaster_Write(_i2c, address, &reg, 1);
+
+    uint8_t recv[1];
+    if (!I2CMaster_Read(_i2c, address, recv, sizeof(recv))) return -1;
+
+    *val = recv[0];
+
+    return 1;
+}
+
+/*!
+ *  @brief  Gets the confirming PWM value in registers -> DEBUG
+ *  @param  num One of the PWM output pins, from 0 to 15
+ *  @desc All values are sent to serial and should be 0 after successful write.
+ */
+static void readPWM(int _i2c, uint8_t num) {
+    int test = 0;
+    Log_Debug("PWM reg %d: ", num);
+    for (int i = 1; i < 5; i++) {
+        readReg8(_i2c, PCA9685_I2C_ADDRESS, (PCA9685_LED0_ON_L + 4 * num) + i, &test);
+        Log_Debug("%d; ", test);
+    }
+    Log_Debug("\n");
+}
+
+/*!
+ *  @brief  Sets the PWM output of one of the PCA9685 pins
+ *  @param _i2c I2C interface
+ *  @param  num One of the PWM output pins, from 0 to 15
+ *  @param  on At what point in the 4096-part cycle to turn the PWM output ON
+ *  @param  off At what point in the 4096-part cycle to turn the PWM output OFF
+ *  @return Number of sent bytes
+ */
+static int setPWM(int _i2c, uint8_t num, uint16_t on, uint16_t off) {
+    uint8_t send[5];
+    send[0] = PCA9685_LED0_ON_L + 4 * num;
+    send[1] = on;
+    send[2] = on >> 8;
+    send[3] = off;
+    send[4] = off >> 8;
+
+    int er = I2CMaster_Write(_i2c, PCA9685_I2C_ADDRESS, send, sizeof(send));
+    return er;
+}
+
+/*
+***** Init functions
+*/
+
+/*!
+ *  @brief  Setups the i2c interface
+ *  @param port i2c port on azure sphere
+ *  @param _i2c 
+ *  @return 1 if success -1 otherwise
+ */
+static int initI2C(int port, int *_i2c) {
+    *_i2c = I2CMaster_Open(port);
+    if (*_i2c == -1) {
+        Log_Debug("Cannot open i2c!\n");
+    }
+
+    if (I2CMaster_SetBusSpeed(*_i2c, I2C_BUS_SPEED_STANDARD) != 0) {
+        Log_Debug("ERROR: Failed to set I2C bus speed: errno=%d (%s)\n",
+            errno, strerror(errno));
+        return -1;
+    }
+
+    if (I2CMaster_SetTimeout(*_i2c, I2C_BUS_TIMEOUT_MS) != 0) {
+        Log_Debug("ERROR: I2CMaster_SetTimeout: errno=%d (%s)\n",
+            errno, strerror(errno));
+        return -1;
+    }
+
+    return 1;
+}
+
+/*!
+ *  @brief  Setups internal registers in PCA9685 for drive
+ *  @param _i2c I2C interface
+ *  @return 1 on success
+ */
+static int initBoard(int _i2c) {
+    uint8_t mode = 0, err = 0;
+    do {
+        err = writeReg8(_i2c, PCA9685_I2C_ADDRESS, PCA9685_MODE1, 0);
+        if (err <= 0) 
+            Log_Debug("Cannot write restart! err: %d\n", err); // err 0 : No bytes sent; -1 : not connected
+
+        // If byte was not received try again
+        readReg8(_i2c, PCA9685_I2C_ADDRESS, PCA9685_MODE1, &mode);
+        if(mode != 0)
+            Log_Debug("Incorrect mode: %d\n Trying again...\n", mode);
+
+    } while (mode != 0);
+
+    uint8_t oldmode;
+    readReg8(_i2c, PCA9685_I2C_ADDRESS, PCA9685_MODE1, &oldmode);
+    uint8_t newmode = (oldmode & ~MODE1_RESTART) | MODE1_SLEEP; // Prepare sleep byte --> config mode
+
+    // Set prescale
+    writeReg8(_i2c, PCA9685_I2C_ADDRESS, PCA9685_MODE1, newmode); // Send sleep byte
+    writeReg8(_i2c, PCA9685_I2C_ADDRESS, PCA9685_PRESCALE, 200); // Set prescale to MAX (PWM Period)
+    writeReg8(_i2c, PCA9685_I2C_ADDRESS, PCA9685_MODE1, oldmode); // Wake up
+
+    // This sets the MODE1 register to turn on auto increment.
+    writeReg8(_i2c, PCA9685_I2C_ADDRESS, PCA9685_MODE1, oldmode | MODE1_RESTART | MODE1_AI);
+
+    return 1;
+}
+
+/*
+***** High-level interface 
+*/
+
+/*!
+ *  @brief  Clamps control variables for PWM
+ *  @param val Value to clamp
+ *  @param on On duty cycle for PWM
+ *  @param off Off duty cycle for PWM
+ */
+void controlVars(uint16_t val, uint16_t *on, uint16_t *off) {
+    if (val >= 4096) {
+        *on = 4096;
+        *off = 0;
+    }
+    else if (val <= 0) {
+        *on = 0;
+        *off = 4096;
+    }
+    else {
+        *on = 0;
+        *off = val;
+    }
+}
+
+/*!
+ *  @brief  Spins all motors forward
+ *  @param _i2c I2C interface
+ *  @param val Speed regulation 0 - stop; 4096 - max
+ */
+void forward(int _i2c, uint16_t val) {
+    uint16_t on, off;
+    controlVars(val, &on, &off);
+    setPWM(_i2c, LEFT1F, on, off);
+    setPWM(_i2c, LEFT2F, on, off);
+    setPWM(_i2c, LEFT3F, on, off);
+    setPWM(_i2c, RIGHT1F, on, off);
+    setPWM(_i2c, RIGHT2F, on, off);
+    setPWM(_i2c, RIGHT3F, on, off);
+}
+
+/*!
+ *  @brief  Spins all motors backward
+ *  @param _i2c I2C interface
+ *  @param val Speed regulation 0 - stop; 4096 - max
+ */
+void backward(int _i2c, uint16_t val) {
+    uint16_t on, off;
+    controlVars(val, &on, &off);
+    setPWM(_i2c, LEFT1B, on, off);
+    setPWM(_i2c, LEFT2B, on, off);
+    setPWM(_i2c, LEFT3B, on, off);
+    setPWM(_i2c, RIGHT1B, on, off);
+    setPWM(_i2c, RIGHT2B, on, off);
+    setPWM(_i2c, RIGHT3B, on, off);
+}
+
+/*!
+ *  @brief  Stops all motors
+ *  @param _i2c I2C interface
+ */
+void stop(int _i2c) {
+    for (int i = 0; i < 16; i++) {
+        setPWM(_i2c, i, 0, 0);
+    }
+}
+
+/*!
+ *  @brief  Spins left motors forward and right motors backward, turning right
+ *  @param _i2c I2C interface
+ *  @param val Speed regulation 0 - stop; 4096 - max
+ */
+void right(int _i2c, uint16_t val) {
+    uint16_t on, off;
+    controlVars(val, &on, &off);
+    setPWM(_i2c, LEFT1F, on, off);
+    setPWM(_i2c, LEFT2F, on, off);
+    setPWM(_i2c, LEFT3F, on, off);
+
+    setPWM(_i2c, RIGHT1B, on, off);
+    setPWM(_i2c, RIGHT2B, on, off);
+    setPWM(_i2c, RIGHT3B, on, off);
+}
+
+/*!
+ *  @brief  Spins right motors forward and left motors backward, turning left
+ *  @param _i2c I2C interface
+ *  @param val Speed regulation 0 - stop; 4096 - max
+ */
+void left(int _i2c, uint16_t val) {
+    uint16_t on, off;
+    controlVars(val, &on, &off);
+    setPWM(_i2c, LEFT1B, on, off);
+    setPWM(_i2c, LEFT2B, on, off);
+    setPWM(_i2c, LEFT3B, on, off);
+
+    setPWM(_i2c, RIGHT1F, on, off);
+    setPWM(_i2c, RIGHT2F, on, off);
+    setPWM(_i2c, RIGHT3F, on, off);
+}
+
+int main() {
     const struct timespec sleepTime = {1, 0};
 
-    char msg[80];
-    while (true) {
-        GroveTempHumiSHT31_Read(sht31);
-        float temp = GroveTempHumiSHT31_GetTemperature(sht31);
-        float humi = GroveTempHumiSHT31_GetHumidity(sht31);
+    // Init I2C and PCA
+    int _i2c;
+    initI2C(0, &_i2c);
+    initBoard(_i2c);
 
-        snprintf(msg, 80, "%f, %f\n", temp, humi);
-        Log_Debug(msg);
+    while (1) {
+        // Go slowly forward for 1 sec
+        forward(_i2c, 300);
+        nanosleep(&sleepTime, NULL);
+
+        // Go slowly backward for 1 sec
+        stop(_i2c); // Always clean pins until you take another action!
+        backward(_i2c, 200);
+        nanosleep(&sleepTime, NULL);
+
+        // Stop for 1 sec
+        stop(_i2c);
         nanosleep(&sleepTime, NULL);
     }
 }
